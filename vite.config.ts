7651af8e0f63d37607a76ec1483efa8b143d950c
Michael Vogtherr <vogtherr.michael@gmail.com>
2025-04-13 14:15:46 +0200

    Add env specific base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // <-- WICHTIG!
+export default defineConfig(({ mode }) => {
+  let base = '/'
+
+  if (mode === 'test') {
+    base = '/svg-encoder/'
+  }
+
+  return {
+    plugins: [react()],
+    base,
+  }
 })
